2b5846b29d64adbf283a6b66a76f0d044797f9f8
kerkeniaziz <kerkeniaziz@gmail.com>
2024-01-21 19:42:33 +0100

    multiple async steps

--- index.js
+++ index.js
@@ -9,7 +9,17 @@ console.log('file written!');
 */
 
 //Non-Blocking, Async way
-fs.readFile('./starter/txt/start.txt','utf-8' , (err,data)=> {
-    console.log(data);
+fs.readFile('./starter/txt/start.txt','utf-8' , (err,data1)=> {
+    fs.readFile(`./starter/txt/${data1}.txt`,'utf-8' , (err,data2)=> {
+    console.log(data2);
+    fs.readFile(`./starter/txt/append.txt`,'utf-8' , (err,data3)=> {
+        console.log(data3);
+
+        fs.writeFile('./starter/txt/final.txt', `${data2} \n ${data3}` ,'utf-8', err=>{
+            console.log('your file has been writthen');
+
+        })
+    });
+});
 });
 console.log('will read file!');
